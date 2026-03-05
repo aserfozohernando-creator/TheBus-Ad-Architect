90841c0b6d333817a9213db33b03494d9e554fd5
aserfozohernando-creator <aserfozohernando@gmail.com>
2026-03-05 08:04:20 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,29 @@
-const CACHE_NAME = 'bus-ads-v1';
+const CACHE_NAME = 'bus-ads-v1.2';
 const ASSETS = [
-  './advertising.html',
+  './',
+  './index.html',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
   'https://code.jquery.com/jquery-3.7.1.min.js',
   'https://cdn.jsdelivr.net/npm/chart.js'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      // We use map to catch individual errors so the whole thing doesn't crash
+      return Promise.all(
+        ASSETS.map(url => {
+          return cache.add(url).catch(err => console.error('Fetch failed for:', url));
+        })
+      );
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
